604f9d3a96870a3fc965c1d5e4f3b245efca16be
matthewking4 <matthewking4@sky.uk>
2019-12-20 22:49:55 +0000

    Connect to a DB

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route exact path="/signup">
+          <App />
+        </Route>
+        <Route exact path="/booking">
+          <App />
+        </Route>
+        <Route exact path="/about">
+          <App />
+        </Route>
+        <Route path="/*">
+          <div>
+            <p>404 - Something went wrong :(</p>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,51 @@
-
 import cors from 'cors';
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
-const PORT = 443
+const port = 443
+
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/config', (req, res) => {
   res.send({features:{cuck:true}});
 });
 
-app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
-);
+app.post('/signup', (req, res) => {
+  if (req.body && req.body.accountName && req.body.accountPassword){
+    res.end("signup Sucessful")
+  }
+  res.status(400).json({ error: 'no credentials posted' })
+})
+
+connect();
+
+function listen() {
+  // var BookSchema = mongoose.Schema({
+  //   name: String,
+  //   price: Number,
+  //   quantity: Number
+  // });
+  // var Book = mongoose.model('enigmatest', BookSchema);
+  // var book1 = new Book({ name: 'a fucker and his database 2', price: 10, quantity: 25 });
+  // book1.save(function (err, book) {
+  //   if (err) return console.error(err);
+  //   console.log(book.name + " saved to bookstore collection.");
+  // });
+  // Book.findById('5dfd4ec4f30c6e1eecfa5f4e', function(err, arr) {console.log(err, arr)})
+  // Book.update({_id:'5dfd4ec4f30c6e1eecfa5f4e' }, {name: 'A fucker and his dattabase Part 3'}, { multi: false }, function(err){ console.log(err)})
+  app.listen(port);
+  console.log('App started on port ' + port);
+}
+
+function connect() {
+  mongoose.connection
+    .on('error', console.log)
+    .on('disconnected', connect)
+    .once('open', listen);
+  return mongoose.connect('mongodb://35.246.119.118:27017/enigmatest', { keepAlive: 1, useNewUrlParser: true });
+}
